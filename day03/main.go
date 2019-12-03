513591c6e3c81950f8e6ffbca88812f9e743b41d
Harald Wartig <hwartig@gmail.com>
2019-12-03 22:35:49 +0100

    Add day 3 part 2 solution

--- day03/main.go
+++ day03/main.go
@@ -16,49 +16,50 @@ type Step struct {
 }
 
 type Point struct {
-	X int
-	Y int
+	X     int
+	Y     int
+	Steps int
 }
 
-func (a Point) Compare (b Point) int {
-  if a.ManhattanDistance() < b.ManhattanDistance() {
-    return -1
-  } 
+func (a Point) Compare(b Point) int {
+	if a.ManhattanDistance() < b.ManhattanDistance() {
+		return -1
+	}
 
-  if a.ManhattanDistance() > b.ManhattanDistance() {
-    return 1
-  }
+	if a.ManhattanDistance() > b.ManhattanDistance() {
+		return 1
+	}
 
-  if a.X < b.X {
-    return -1
-  }
+	if a.X < b.X {
+		return -1
+	}
 
-  if a.X > b.X {
-    return 1
-  }
+	if a.X > b.X {
+		return 1
+	}
 
-  if a.Y < b.Y {
-    return -1
-  }
-  
-  if a.Y > b.Y {
-    return 1
-  }
+	if a.Y < b.Y {
+		return -1
+	}
 
-  return 0
+	if a.Y > b.Y {
+		return 1
+	}
+
+	return 0
 }
 
 type Points []Point
 
 func (p Points) Len() int {
-  return len(p)
+	return len(p)
 }
 func (p Points) Swap(i, j int) {
-  p[i], p[j] = p[j], p[i]
+	p[i], p[j] = p[j], p[i]
 }
 
 func (p Points) Less(i, j int) bool {
-  return p[i].Compare(p[j]) < 0
+	return p[i].Compare(p[j]) < 0
 }
 
 func abs(x int) int {
@@ -68,16 +69,18 @@ func abs(x int) int {
 	return x
 }
 
-func(p Point) ManhattanDistance() int {
-  return abs(p.X) + abs(p.Y)
+func (p Point) ManhattanDistance() int {
+	return abs(p.X) + abs(p.Y)
 }
 
 type Wire struct {
-  Steps []Step
+	Steps []Step
 }
 
 func (w Wire) Points() (points Points) {
 	x, y := 0, 0 // starting position
+
+	l := 1 // wire length, distance from wire start
 
 	for _, step := range w.Steps {
 		for i := 0; i < step.Distance; i += 1 {
@@ -92,10 +95,11 @@ func (w Wire) Points() (points Points) {
 				x = x - 1
 			}
 
-			points = append(points, Point{x, y})
+			points = append(points, Point{x, y, l})
+			l = l + 1
 		}
 	}
-  sort.Sort(points)
+	sort.Sort(points)
 
 	return
 }
@@ -106,46 +110,64 @@ func parseWire(input string) (wire Wire) {
 	for _, part := range parts {
 		direction := string(part[0])
 		number, _ := strconv.Atoi(part[1:])
-    wire.Steps = append(wire.Steps, Step{direction, number})
+		wire.Steps = append(wire.Steps, Step{direction, number})
 	}
 
 	return
 }
 
-func (a Wire) FindOverlaps(b Wire) (points Points) {
-  ap := a.Points()
-  bp := b.Points()
+func (a Wire) FindIntersections(b Wire) (intersections []Intersection) {
+	ap := a.Points()
+	bp := b.Points()
 
-  for i, j := 0, 0; i < len(ap) && j < len(bp); {
-    //fmt.Printf("%d: %v < %d: %v\n", i, ap[i], j, bp[j])
+	for i, j := 0, 0; i < len(ap) && j < len(bp); {
+		//fmt.Printf("%d: %v < %d: %v\n", i, ap[i], j, bp[j])
 
-    switch(ap[i].Compare(bp[j])) {
-    case -1:
-      i = i + 1
-    case 0:
-      points = append(points, ap[i])
-      i = i + 1
-      j = j + 1
-    case 1:
-      j = j + 1
-    } 
-  }
+		switch ap[i].Compare(bp[j]) {
+		case -1:
+			i = i + 1
+		case 0:
+			intersections = append(intersections, Intersection{ap[i], bp[j]})
+			i = i + 1
+			j = j + 1
+		case 1:
+			j = j + 1
+		}
+	}
 
-  return 
+	return
+}
+
+type Intersection struct {
+	A Point
+	B Point
+}
+
+func (in Intersection) Steps() int {
+	return in.A.Steps + in.B.Steps
 }
 
 func main() {
-  file, err := os.Open("input.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal("couldn't open file, ", err)
 	}
-  defer file.Close()
+	defer file.Close()
 
-  s := bufio.NewScanner(file)
-  s.Scan()
-  wire1 := parseWire(s.Text())
-  s.Scan()
-  wire2 := parseWire(s.Text())
+	s := bufio.NewScanner(file)
+	s.Scan()
+	wire1 := parseWire(s.Text())
+	s.Scan()
+	wire2 := parseWire(s.Text())
 
-  fmt.Println(wire1.FindOverlaps(wire2)[0].ManhattanDistance())
+	intersections := wire1.FindIntersections(wire2)
+
+	// part 1
+	fmt.Println("part1: ", intersections[0].A.ManhattanDistance())
+
+	// part 2
+	sort.Slice(intersections, func(i, j int) bool {
+		return intersections[i].Steps() < intersections[j].Steps()
+	})
+	fmt.Println("part2: ", intersections[0].Steps())
 }
--- day03/main_test.go
+++ day03/main_test.go
@@ -34,8 +34,8 @@ func (a Points) Equal(b Points) bool {
 
 func TestParseWire(t *testing.T) {
 	testCases := map[string]Wire{
-    "R8":          Wire{[]Step{{"R", 8}}},
-    "R8,U5,L5,D3": Wire{[]Step{{"R", 8}, {"U", 5}, {"L", 5}, {"D", 3}}},
+		"R8":          Wire{[]Step{{"R", 8}}},
+		"R8,U5,L5,D3": Wire{[]Step{{"R", 8}, {"U", 5}, {"L", 5}, {"D", 3}}},
 	}
 
 	for input, expected := range testCases {
@@ -49,11 +49,11 @@ func TestParseWire(t *testing.T) {
 
 func TestWirePointCloud(t *testing.T) {
 	testCases := map[string]Points{
-		"R3":          Points{{1, 0}, {2, 0}, {3, 0}},
-		"U3":          Points{{0, 1}, {0, 2}, {0, 3}},
-		"L3":          Points{{-1, 0}, {-2, 0}, {-3, 0}},
-		"D3":          Points{{0, -1}, {0, -2}, {0, -3}},
-		"R1,U1,L2,D2": Points{{-1,0}, {0, 1}, {1,0}, {-1,-1}, {-1,1}, {1,1}},
+		"R3":          Points{{1, 0, 1}, {2, 0, 2}, {3, 0, 3}},
+		"U3":          Points{{0, 1, 1}, {0, 2, 2}, {0, 3, 3}},
+		"L3":          Points{{-1, 0, 1}, {-2, 0, 2}, {-3, 0, 3}},
+		"D3":          Points{{0, -1, 1}, {0, -2, 2}, {0, -3, 3}},
+		"R1,U1,L2,D2": Points{{-1, 0, 5}, {0, 1, 3}, {1, 0, 1}, {-1, -1, 6}, {-1, 1, 4}, {1, 1, 2}},
 	}
 
 	for input, expected := range testCases {
@@ -63,5 +63,5 @@ func TestWirePointCloud(t *testing.T) {
 		if !actual.Equal(expected) {
 			t.Errorf("%v: Expected: %v but got: %v", input, expected, actual)
 		}
-  }
+	}
 }
