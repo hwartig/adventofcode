b1489799171ad3b4e13239cde5850a01af81f4e7
Harald Wartig <hwartig@gmail.com>
2019-12-04 22:38:44 +0100

    Add day 4 part 2 solution

--- day04/main.go
+++ day04/main.go
@@ -5,7 +5,37 @@ import (
 	"strconv"
 )
 
-func meetsCriteria(i int) bool {
+type Criteria = func(a int) bool
+
+func meetsCriteriaPart2(input int) bool {
+	digits := strconv.Itoa(input)
+	hasDoubleDigits := false
+
+	for i := 1; i < len(digits); i++ {
+		//Two adjacent digits are the same (like 22 in 122345).
+		if digits[i-1] == digits[i] {
+			// only count group if its not part of a tripple
+			if i == 1 && digits[i] != digits[i+1] {
+				hasDoubleDigits = true
+			}
+			if i > 1 && i < len(digits)-1 && digits[i-1] != digits[i-2] && digits[i] != digits[i+1] {
+				hasDoubleDigits = true
+			}
+			if i == len(digits)-1 && digits[i-1] != digits[i-2] {
+				hasDoubleDigits = true
+			}
+		}
+
+		//Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+		if digits[i-1] > digits[i] {
+			return false
+		}
+	}
+
+	return hasDoubleDigits
+}
+
+func meetsCriteriaPart1(i int) bool {
 	digits := strconv.Itoa(i)
 	hasDoubleDigits := false
 
@@ -24,11 +54,11 @@ func meetsCriteria(i int) bool {
 	return hasDoubleDigits
 }
 
-func countValidPasswords(from, to int) int {
+func countValidPasswords(from, to int, c Criteria) int {
 	count := 0
 
 	for i := from; i <= to; i++ {
-		if meetsCriteria(i) {
+		if c(i) {
 			count += 1
 		}
 	}
@@ -36,5 +66,6 @@ func countValidPasswords(from, to int) int {
 }
 
 func main() {
-	fmt.Println("part1: ", countValidPasswords(246540, 787419))
+	//fmt.Println("part1: ", countValidPasswords(246540, 787419, meetsCriteriaPart1))
+	fmt.Println("part2: ", countValidPasswords(246540, 787419, meetsCriteriaPart2))
 }
--- day04/main_test.go
+++ day04/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestMeetsCriteria(t *testing.T) {
+func TestMeetsCriteriaPart1(t *testing.T) {
 	testCases := map[int]bool{
 		111111: true,
 		223450: false,
@@ -11,7 +11,23 @@ func TestMeetsCriteria(t *testing.T) {
 	}
 
 	for input, expected := range testCases {
-		actual := meetsCriteria(input)
+		actual := meetsCriteriaPart1(input)
+		if actual != expected {
+			t.Errorf("%v: Expected: %v, got: %v", input, expected, actual)
+		}
+	}
+}
+
+func TestMeetsCriteriaPart2(t *testing.T) {
+	testCases := map[int]bool{
+		112233: true,
+		123444: false,
+		111234: false,
+		111122: true,
+	}
+
+	for input, expected := range testCases {
+		actual := meetsCriteriaPart2(input)
 		if actual != expected {
 			t.Errorf("%v: Expected: %v, got: %v", input, expected, actual)
 		}
