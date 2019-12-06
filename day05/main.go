09908d37e172f58b2814e55f6fb6c50608b552ed
Harald Wartig <hwartig@gmail.com>
2019-12-06 21:06:39 +0100

    add day 5 part 2 solution

--- day05/main.go
+++ day05/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -16,20 +19,20 @@ type instruction struct {
 	Mode1, Mode2, Mode3    int
 }
 
-func parseInstruction(input []int) instruction {
+func parseInstruction(mem []int) instruction {
 	var p1, p2, p3 int
 
-	if len(input) > 1 {
-		p1 = input[1]
+	if len(mem) > 1 {
+		p1 = mem[1]
 	}
-	if len(input) > 2 {
-		p2 = input[2]
+	if len(mem) > 2 {
+		p2 = mem[2]
 	}
-	if len(input) > 3 {
-		p3 = input[3]
+	if len(mem) > 3 {
+		p3 = mem[3]
 	}
 
-	modesAndOptCode := fmt.Sprintf("%05d", input[0])
+	modesAndOptCode := fmt.Sprintf("%05d", mem[0])
 	// mode flags are in decreasing order from left to right
 	mode1, _ := strconv.Atoi(string(modesAndOptCode[2]))
 	mode2, _ := strconv.Atoi(string(modesAndOptCode[1]))
@@ -47,57 +50,90 @@ func parseInstruction(input []int) instruction {
 	}
 }
 
-func resolveParams(instr *instruction, input []int, count int) {
+func resolveParams(instr *instruction, mem []int, count int) {
 	if count > 0 && instr.Mode1 == 0 { // not really needed. why would you call the method if count == 0
-		instr.Param1 = input[instr.Param1]
+		instr.Param1 = mem[instr.Param1]
 	}
 	if count > 1 && instr.Mode2 == 0 {
-		instr.Param2 = input[instr.Param2]
+		instr.Param2 = mem[instr.Param2]
 	}
 	if count > 2 && instr.Mode3 == 0 {
-		instr.Param3 = input[instr.Param3]
+		instr.Param3 = mem[instr.Param3]
 	}
 }
 
-func run(s string, noun, verb int) []int {
-	input := splitToNumbers(s)
+func run(s string, in io.Reader, out io.Writer) []int {
+	mem := splitToNumbers(s)
 
 	// add some padding to the end as we're parsing instrs in batches of 4
-	input = append(input, 0, 0, 0)
-
-	// add overrides to original input
-	if noun > 0 {
-		input[1] = noun
-	}
-	if verb > 0 {
-		input[2] = verb
-	}
+	mem = append(mem, 0, 0, 0)
 
 	pos := 0
 	for {
-		instr := parseInstruction(input[pos : pos+4])
-		// log.Println("instruction: ", instr)
+		instr := parseInstruction(mem[pos : pos+4])
 
 		switch instr.OpCode {
 		case 99: // halt
-			return input[:len(input)-3] // return without extra padding
+			return mem[:len(mem)-3] // return without extra padding
 		case 1: // addition
-			resolveParams(&instr, input, 2)
-			input[instr.Param3] = instr.Param1 + instr.Param2
+			resolveParams(&instr, mem, 2)
+			mem[instr.Param3] = instr.Param1 + instr.Param2
 			pos = pos + 4
 		case 2: // multiplication
-			resolveParams(&instr, input, 2)
-			input[instr.Param3] = instr.Param1 * instr.Param2
+			resolveParams(&instr, mem, 2)
+			mem[instr.Param3] = instr.Param1 * instr.Param2
 			pos = pos + 4
 		case 3: // input
-			fmt.Scan(&input[instr.Param1])
+			s := bufio.NewScanner(in)
+			if s.Scan() {
+				n, err := strconv.Atoi(s.Text())
+				if err != nil {
+					log.Fatal("Unable to read input, ", err)
+				}
+				mem[instr.Param1] = n
+			}
 
 			pos = pos + 2
 		case 4: // output
-			resolveParams(&instr, input, 1)
-			fmt.Println(instr.Param1)
+			resolveParams(&instr, mem, 1)
+
+			_, err := io.WriteString(out, strconv.Itoa(instr.Param1))
+
+			if err != nil {
+				log.Fatal("Unable to write to output, ", err)
+			}
 
 			pos = pos + 2
+		case 5: // jump if true
+			resolveParams(&instr, mem, 2)
+			if instr.Param1 != 0 {
+				pos = instr.Param2
+			} else {
+				pos = pos + 3
+			}
+		case 6: // jump if false
+			resolveParams(&instr, mem, 2)
+			if instr.Param1 == 0 {
+				pos = instr.Param2
+			} else {
+				pos = pos + 3
+			}
+		case 7: // less than
+			resolveParams(&instr, mem, 2)
+			if instr.Param1 < instr.Param2 {
+				mem[instr.Param3] = 1
+			} else {
+				mem[instr.Param3] = 0
+			}
+			pos = pos + 4
+		case 8: // equals
+			resolveParams(&instr, mem, 2)
+			if instr.Param1 == instr.Param2 {
+				mem[instr.Param3] = 1
+			} else {
+				mem[instr.Param3] = 0
+			}
+			pos = pos + 4
 		default:
 			log.Fatalf("Unknown opcode %v in instruction %v\n", instr.OpCode, instr)
 		}
@@ -121,6 +157,6 @@ func main() {
 		log.Fatal("couldn't read file: ", fileName, err)
 	}
 
-	// run("3,0,4,0,99", 0, 0)
-	run(string(content), 0, 0)
+	run("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", os.Stdin, os.Stdout)
+	run(string(content), os.Stdin, os.Stdout)
 }
--- day05/main_test.go
+++ day05/main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -23,26 +26,59 @@ func TestSplitToNumbers(t *testing.T) {
 	}
 }
 
-func TestRun(t *testing.T) {
-	testCases := map[string][]int{
-		// indirect mode
-		"1,0,0,0,99":                    []int{2, 0, 0, 0, 99},       // (1 + 1 = 2).
-		"2,3,0,3,99":                    []int{2, 3, 0, 6, 99},       // (3 * 2 = 6).
-		"2,4,4,5,99,0":                  []int{2, 4, 4, 5, 99, 9801}, // (99 * 99 = 9801).
-		"1,1,1,4,99,5,6,0,99":           []int{30, 1, 1, 4, 2, 5, 6, 0, 99},
-		"1,9,10,3,2,3,11,0,99,30,40,50": []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50},
-		"3,0,4,0,99":                    []int{3, 0, 4, 0, 99},
-		// direct mode
-		"1101,2,3,3,99": []int{1101, 2, 3, 5, 99}, // (2 + 3 = 5).
-		"1102,2,3,3,99": []int{1102, 2, 3, 6, 99}, // (2 * 3 = 6).
+type RunTestCase struct {
+	StartMem       string
+	ExpectedEndMem []int
+	Input          string
+	ExpectedOutput string
+}
 
+func (tc RunTestCase) Reader() io.Reader {
+	return strings.NewReader(tc.Input)
+}
+
+func TestRunInputOutput(t *testing.T) {
+
+	testCases := []RunTestCase{
+		// positional mode
+		{"1,0,0,0,99", []int{2, 0, 0, 0, 99}, "", ""},         // (1 + 1 = 2).
+		{"2,3,0,3,99", []int{2, 3, 0, 6, 99}, "", ""},         // (3 * 2 = 6).
+		{"2,4,4,5,99,0", []int{2, 4, 4, 5, 99, 9801}, "", ""}, // (99 * 99 = 9801).
+		{"1,1,1,4,99,5,6,0,99", []int{30, 1, 1, 4, 2, 5, 6, 0, 99}, "", ""},
+		{"1,9,10,3,2,3,11,0,99,30,40,50", []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}, "", ""},
+
+		{"3,5,4,5,99,0", []int{3, 5, 4, 5, 99, 1}, "1", "1"}, // input => output
+
+		{"3,9,8,9,10,9,4,9,99,-1,8", []int{3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8}, "1", "0"},  // equal
+		{"3,9,8,9,10,9,4,9,99,-1,8", []int{3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8}, "8", "1"},  // equal
+		{"3,9,7,9,10,9,4,9,99,-1,8", []int{3, 9, 7, 9, 10, 9, 4, 9, 99, 1, 8}, "1", "1"},  // less than
+		{"3,9,7,9,10,9,4,9,99,-1,8", []int{3, 9, 7, 9, 10, 9, 4, 9, 99, 0, 8}, "10", "0"}, // less than
+
+		{"3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 1, 1, 9}, "-1", "1"}, // jumps
+		{"3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 0, 0, 1, 9}, "0", "0"},   // jumps
+
+		// immediate mode
+		{"1101,2,3,3,99", []int{1101, 2, 3, 5, 99}, "", ""}, // (2 + 3 = 5).
+		{"1102,2,3,3,99", []int{1102, 2, 3, 6, 99}, "", ""}, // (2 * 3 = 6).
+
+		{"3,3,1108,-1,8,3,4,3,99", []int{3, 3, 1108, 0, 8, 3, 4, 3, 99}, "1", "0"},  // equal
+		{"3,3,1108,-1,8,3,4,3,99", []int{3, 3, 1108, 1, 8, 3, 4, 3, 99}, "8", "1"},  // equal
+		{"3,3,1107,-1,8,3,4,3,99", []int{3, 3, 1107, 1, 8, 3, 4, 3, 99}, "1", "1"},  // less than
+		{"3,3,1107,-1,8,3,4,3,99", []int{3, 3, 1107, 0, 8, 3, 4, 3, 99}, "10", "0"}, // less than
+
+		{"3,3,1105,-1,9,1101,0,0,12,4,12,99,1", []int{3, 3, 1105, -7, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, "-7", "1"}, // jumps
+		{"3,3,1105,-1,9,1101,0,0,12,4,12,99,1", []int{3, 3, 1105, 0, 9, 1101, 0, 0, 12, 4, 12, 99, 0}, "0", "0"},   // jumps
 	}
 
-	for input, expected := range testCases {
-		actual := run(input, 0, 0)
+	for _, tc := range testCases {
+		var buf bytes.Buffer
+		actual := run(tc.StartMem, tc.Reader(), &buf)
 
-		if !reflect.DeepEqual(expected, actual) {
-			t.Errorf("%v: Expected: %v but got: %v", input, expected, actual)
+		if !reflect.DeepEqual(tc.ExpectedEndMem, actual) {
+			t.Errorf("%v: Expected End Mem differs: %v but got: %v", tc.StartMem, tc.ExpectedEndMem, actual)
+		}
+		if buf.String() != tc.ExpectedOutput {
+			t.Errorf("%v: Expected Output differs: %v but got: %v", tc.StartMem, tc.ExpectedOutput, buf.String())
 		}
 	}
 }
